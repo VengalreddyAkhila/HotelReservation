87dbaa4680a7ad6470eb576b589ce0ff33ec5342
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-08-06 23:54:00 +0530

    [akhila]Refactor:added the json file to write the data

--- hotel.js
+++ hotel.js
@@ -1,26 +1,46 @@
 const hotel = require("./hotel.json");
 const fs = require("fs");
-hotel.push({
-    "id":2,
-      "name":"TajHotel",
-      "numberofrooms":10,
-      "Beds":"doublebed",
-      "price":1500
-})
-console.log(hotel);
-//to write the file in json
-fs.writeFile('hotel.json',JSON.stringify(hotel),(err) => {
-    if (err) {
-        console.log(`Error : ${err}`);
-    } else {
-        console.log(`File is written successfully!`);
+class Hotel {
+    constructor (id,name,numberofrooms,Beds,price)
+    {
+        this.id = id;
+        this.name = name;
+        this.numberofrooms = numberofrooms;
+        this.Beds = Beds;
+        this.price = price;
     }
-});
+    checkprice(){
+        console.log(this.name,this.id)
+    }
+}
+let hoteldata = new Hotel(1,"Banjara",10,"doublebed",1500);
+let taj = new Hotel(4,"paradise",9,"single",7890);
+hoteldata.checkprice();
+taj.checkprice();
+console.log(taj);
 
-fs.readFile('./hotel.json', 'utf8', (err, jsonString) => {
-    if (err) {
-        console.log("File read failed:", err)
-        return
-    }
-    console.log('File data:', jsonString) 
+//read the file 
+fs.readFile('hotel1.json', function (err, data) {
+    var json = JSON.parse(data)
+    json.push(taj)
+//write the file in json
+    fs.writeFile("hotel1.json", JSON.stringify(json), (err) =>{
+        console.log(err)
+    })
 })
+
+var res1 = "";
+   var res2 = "";
+   for (var i in hoteldata.Beds) {
+      res1 += hoteldata.Beds[i] 
+   }
+   console.log("Before change: " +" "+res1);
+   
+   hoteldata.Beds[0] = "singlebed";
+   for (var i in hoteldata.Beds) {
+      res2 += hoteldata.Beds[i] 
+   }
+   console.log("After change:" +" "+res2);
+
+
+
--- users.js
+++ users.js
@@ -1,37 +1,47 @@
 const users = require("./users.json");
 const fs = require("fs");
-users.push({
-        "id":5,
-        "name":"Ram",
-        "phonenumber":9234456719,
-        "emailid":"ram@gmail.com",
-        "address":"Mumbai",
-        "country":"India",
-        "bookedid":[2,3,7,10]
-},
-{
-    "id":6,
-        "name":"Naveen",
-        "phonenumber":8334456719,
-        "emailid":"nav@gmail.com",
-        "address":"Maharastra",
-        "country":"India",
-        "bookedid":[2,8,3,20]
+class Users{
+  constructor(id,name,phonenumber,emailid,address,country,bookedid){
+    this.id = id;
+    this.name = name;
+    this.phonenumber = phonenumber;
+    this.emailid = emailid;
+    this.address = address;
+    this.country = country;
+    this.bookedid = bookedid;
+  }
+  checkprice(){
+    console.log(this.name);
+  }
+}
+let usersdata = new Users(2,"Akhi",8247236922,"akki@gmail.com","Hyd","India",[23,21]);
+let getdata = new Users(3,"banu",9087654323,"as@gmail.com","Vizag","India",24);
+usersdata.checkprice();
+getdata.checkprice();
+console.log(usersdata);
+//read the file 
+fs.readFile('booking.json', function (err, data) {
+    var json = JSON.parse(data)
+   // console.log(typeof(Beds))
+    json.push(usersdata.bookedid)
+//write the file in json
+    fs.writeFile("booking.json", JSON.stringify(json), (err) =>{
+        console.log(err)
+    })
 })
-console.log(users);
-//to write the file in json
-fs.writeFile('users.json',JSON.stringify(users),(err) => {
-    if (err) {
-        console.log(`Error : ${err}`);
-    } else {
-        console.log(`File is written successfully!`);
-    }
-});
 
-fs.readFile('./users.json', 'utf8', (err, jsonString) => {
-    if (err) {
-        console.log("File read failed:", err)
-        return
-    }
-    console.log('File data:', jsonString) 
-})
+
+var res1 = "";
+   var res2 = "";
+   for (var i in usersdata.bookedid) {
+      res1 += usersdata.bookedid[i] 
+   }
+   console.log("Before change: " +" "+res1);
+   usersdata.bookedid[0] = "98";
+   usersdata.bookedid[1] = "72";
+   for (var i in usersdata.bookedid) {
+      res2 += usersdata.bookedid[i] 
+   }
+   console.log("After change:" +" "+res2);
+
+
